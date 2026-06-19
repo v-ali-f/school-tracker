dfa44f5678b21792d400a46088b7654d5a4fefe1
Aleksandr <aleksandr@example.local>
2026-06-19 12:03:13 +0300

    Remove deprecated mobile and desktop app clients

--- desktop-app/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('portalApp', {
-  config: () => ipcRenderer.invoke('portal-api:config'),
-  request: (request) => ipcRenderer.invoke('portal-api:request', request),
-  openPortal: () => ipcRenderer.invoke('portal-api:open-portal')
-});
